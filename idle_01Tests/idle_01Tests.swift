f13de35312cd58f9c75127cbcbd59ae8caa1472d
Adam Socki <adam.socki.dev@gmail.com>
2025-09-29 20:59:46 -0500

    Initial Commit

---
+++ idle_01Tests/idle_01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  idle_01Tests.swift
+//  idle_01Tests
+//
+//  Created by Adam Socki on 9/29/25.
+//
+
+import Testing
+@testable import idle_01
+
+struct idle_01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
